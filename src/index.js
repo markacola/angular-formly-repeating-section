bb0c1ee768ce79d9aaab0725bae38b7d018ed6d4
markacola <mark.cola1@gmail.com>
2016-01-25 16:23:05 +1100

    init

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const ngModuleName = 'formlyRepeatingSection';
 const angular = window.angular;
 const ngModule = angular.module(ngModuleName, [ 'angular-formly' ]);
 
-ngModule.constant('formlyBootstrapVersion', VERSION);
-
 import repeatingSection from './repeating-section';
 repeatingSection(ngModule);
 
